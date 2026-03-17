690409924f122c9c900545f2739f5a55c1873e75
Preetkasana001 <kasanapreet436@gmail.com>
2026-03-17 09:59:24 +0000

    variables done

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,14 @@
+const accountId=8926962;
+let accountEmail="preet@gmail.com";
+var accountPassword="8y28";
+accountCity="uttar pradesh";
+
+// accountId=62;
+// const variables cannot be modified
+accountEmail="kasanapreet@gmail.com"
+accountPassword="usis8"
+accountCity="delhi"
+
+
+console.table([accountId,accountPassword,accountCity,accountEmail]);
+
