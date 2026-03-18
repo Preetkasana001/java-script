9de33d2958af4b7f2a56b5104a24efb6bda03f13
Preetkasana001 <kasanapreet436@gmail.com>
2026-03-18 15:59:06 +0000

    datatypes

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,23 @@
+// code readibility should be atmost priority
+"use strict"; //treat all js code as per new version
+// alert(3+3) //we r using node js ,not browser so it will give error 
+
+let name="preet"
+let age=20
+let isLoggedIn="true"
+
+/* bigint,
+string =>""
+boolean =>true/false
+null =>standalone value
+undefined =>value is not assigned
+symbol =>unique
+
+
+objects
+*/
+
+// type of null is object
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
