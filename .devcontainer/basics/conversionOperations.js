1e2adcee14b8e253683e3f83adca355c1091273c
Preetkasana001 <kasanapreet436@gmail.com>
2026-03-22 15:17:19 +0000

    datatype conversion

---
+++ .devcontainer/basics/conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+let something = true
+console.log(typeof score)
+console.log(typeof(score))
+console.log(typeof something)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+let somethingNumber = Number(something)
+console.log(valueInNumber)
+console.log(somethingNumber)
+
+// typeof NaN is number
+// trur =>1, false=>0
+
+let obh = "preet"
+let ghi = Boolean(obh)
+console.log(ghi)
+//  1=>true , 0=>false
+// ""=>false , "preet"=> true
+
+let num = 123
+let numString = String(num)
+console.log(numString)
+console.log(typeof numString)
