44c6982e1a68b9d92114aa2625d9ac7050ca59ab
Preetkasana001 <kasanapreet436@gmail.com>
2026-03-22 15:40:44 +0000

    operators

--- .devcontainer/basics/conversionOperations.js
+++ .devcontainer/basics/conversionOperations.js
@@ -1,25 +1,39 @@
 let score = "33abc"
 let something = true
-console.log(typeof score)
-console.log(typeof(score))
-console.log(typeof something)
+// console.log(typeof score)
+// console.log(typeof(score))
+// console.log(typeof something)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 let somethingNumber = Number(something)
-console.log(valueInNumber)
-console.log(somethingNumber)
+// console.log(valueInNumber)
+// console.log(somethingNumber)
 
 // typeof NaN is number
 // trur =>1, false=>0
 
 let obh = "preet"
 let ghi = Boolean(obh)
-console.log(ghi)
+// console.log(ghi)
 //  1=>true , 0=>false
 // ""=>false , "preet"=> true
 
 let num = 123
 let numString = String(num)
-console.log(numString)
-console.log(typeof numString)
+// console.log(numString)
+// console.log(typeof numString)
+
+
+// ***********operations*************
+ 
+let str1 = "hello"
+let str2 = " preet"
+console.log(str1+str2)
+
+console.log(1+"2")
+console.log("1"+2)
+console.log(1+2+"2")
+console.log("1"+2+1)
+console.log(+true)
+console.log(+"")
