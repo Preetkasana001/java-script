6ab6f1ac4e5be92150147c8523aa3925a7604727
Preetkasana001 <kasanapreet436@gmail.com>
2026-04-11 16:20:35 +0000

    comparing values

---
+++ .devcontainer/basics/04_comparison.js
@@ -0,0 +1,22 @@
+console.log(1>2)
+console.log(1!=2)
+console.log(1<=2)
+console.log(1<2)
+
+// in string and int comaprion the string is converted into int
+
+console.log("1">2)
+console.log("1"<=2)
+console.log("1"!=1)
+
+// if we comapre null andf undefined then it is not suitable as the js convert null and undefined into
+//  NaN and 0 values 
+console.log(null==0)
+console.log(null>0)
+console.log(null>=0)
+
+// === is called the strict comparator which not only compares the value but also the data type of variables
+
+console.log(1===1)
+console.log(1===2)
+console.log("1"===1)
