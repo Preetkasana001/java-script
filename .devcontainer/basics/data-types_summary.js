dd31fca3df734eaf6393129acb96ce390a2f0fc3
Preetkasana001 <kasanapreet436@gmail.com>
2026-04-11 16:35:40 +0000

    data type summary

---
+++ .devcontainer/basics/data-types_summary.js
@@ -0,0 +1,23 @@
+// java script is a dynammicaly typed language
+// primitive data types
+ 
+// string, number, null, undefined, symbol, boolean, BigInt
+
+// refrence (non primitive data type)
+
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
+// console.log(id==anotherId)
+
+// arrar, object, functions
+
+const heros =["iron man","batman","spiderman"];
+
+let MyObj ={
+    name:"preet kasana",
+    age:21,
+}
+
+let MyFunction =function(){
+    console.log("hello world");
+}
