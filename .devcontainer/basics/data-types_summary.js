49f4a491c64cb7d7ba3b00f76f96d011635fafa7
Preetkasana001 <kasanapreet436@gmail.com>
2026-04-14 05:36:55 +0000

    stack&heap basics

--- .devcontainer/basics/data-types_summary.js
+++ .devcontainer/basics/data-types_summary.js
@@ -11,13 +11,49 @@
 
 // arrar, object, functions
 
-const heros =["iron man","batman","spiderman"];
+// const heros =["iron man","batman","spiderman"];
 
-let MyObj ={
-    name:"preet kasana",
-    age:21,
+// let MyObj ={
+//     name:"preet kasana",
+//     age:21,
+// }
+
+// let MyFunction =function(){
+//     console.log("hello world");
+// }
+
+
+// ------------------------------------------------------------------------------------------
+
+
+// memory are stored in two types:-
+// 1->stack(primitive,copy is passed no change in orignal)
+// 2->heap(Non -primitive , refrence is passed orignal value is changed)
+
+
+let MyInstaId = "preetkasana_"
+let AnotherId = MyInstaId
+AnotherId = "kasanPreet436"
+console.log(MyInstaId);
+console.log(AnotherId);
+
+// no change in orignal data as copy is passed
+
+let Mydetail = {
+    name: "Preet kasana",
+    email: "kasanapreet436@gmail.com"
 }
 
-let MyFunction =function(){
-    console.log("hello world");
-}
+let MydetailNew = Mydetail;
+
+Mydetail.email = "preetkasana@gmail.com";
+
+console.log(Mydetail);
+console.log(MydetailNew);
+console.log(Mydetail.email);
+
+// orignal data is also modified in Non primitive 
+
+
+
+
