6da38874300fa61128f6f30faab9032a4d9c9e7a
Preetkasana001 <kasanapreet436@gmail.com>
2026-04-14 11:00:48 +0000

    string methods

---
+++ .devcontainer/basics/05_strings.js
@@ -0,0 +1,26 @@
+const name = "preet kasana"
+
+
+// another way to declare string
+const newWay = new String("Preet-kasana-Gujjar")
+
+const repo = 50
+console.log(name + " "+ repo);
+
+// string indentation is usefull as it is more functional and any space inside it are preserved
+//  and it is more convinient
+console.log(`My name is ${name} and I have ${repo} on github`);
+console.log(name[0]);
+
+// their are many methods like
+// length , uppercase , lowercase , trim , trimend , charAtetc
+console.log(newWay);
+
+console.log(newWay.split("-"))
+console.log(newWay.toUpperCase());
+console.log(newWay.includes('Gujjar'));
+
+
+
+
+
